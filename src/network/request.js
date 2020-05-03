4552b734d47eed161d2921f67534390f3c720190
Damial <18513684268@163.com>
2020-05-03 10:45:16 +0800

    添加事件移除

--- src/network/request.js
+++ src/network/request.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export function request(config) {
   // 1.创建axios的实例
   const instance = axios.create({
-    baseURL: 'http://106.32.32.327:8000/api/h8',
+    baseURL: 'http://106.32.32.339:8000/api/g9',
     timeout: 5000
   })
   // 2.axios的拦截器
