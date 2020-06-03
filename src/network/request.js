d1e3d569bb9e4364c9b0b6e1387bafcaca3d2d5b
Damial <18513684268@163.com>
2020-06-03 21:34:57 +0800

    更新部分

--- src/network/request.js
+++ src/network/request.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export function request(config) {
   // 1.创建axios的实例
   const instance = axios.create({
-    baseURL: 'http://123.207.32.32:8000/api/m3',//接口暂不提供
+    baseURL: 'http://123.307.82.82:8000/api/h6',//接口加老师微信codewhy
 
     timeout: 5000
   })
